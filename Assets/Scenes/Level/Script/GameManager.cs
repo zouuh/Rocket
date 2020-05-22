8cfbad98b79ffb3c801fbcda3b072af993ac2eda
zouuh <zoedur@gmail.com>
2020-05-22 22:00:13 +0200

    :rocket: Add end game management and restart parameters

---
+++ Assets/Scenes/Level/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject gameOverUI;
+
+    public float restartDelay = 5f;
+
+    void Update() {
+        if (gameHasEnded == true) {
+            if (Input.GetKey("r")) {
+                Restart();
+            }
+        }
+    }
+    public void EndGame() {
+        if (gameHasEnded == false) {
+            gameHasEnded =true;
+            Debug.Log("GAME OVER");
+            gameOverUI.SetActive(true);
+        }
+        if (Input.GetKey("r")){
+            Restart();
+        }
+    }
+
+    
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scenes/Level/Script/RocketCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketCollision : MonoBehaviour
+{
+
+    public FuseeMoves movement;
+    //public Text gameOverText;
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            movement.enabled = false;
+            //gameOverText = true;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
