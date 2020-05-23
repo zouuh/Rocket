ea6c625236e78fa32afedbb04d925dbc9b938045
melissandeglt <melissande.gaillot@outlook.fr>
2020-05-23 13:44:06 +0200

    :bug: Fixing asteroid destruction

---
+++ Assets/Scenes/Level/Script/DeleteAsteroid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteAsteroid : MonoBehaviour
+{
+    private float speed = 88.0f;
+    private Rigidbody rb;
+    private Vector3 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Camera.main.transform.position.y, 0));
+        if (transform.position.y+10 < screenBounds.y)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scenes/Level/Script/deployAsteroid.cs
+++ Assets/Scenes/Level/Script/deployAsteroid.cs
@@ -20,7 +20,7 @@ public class deployAsteroid : MonoBehaviour
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(coordX, Camera.main.transform.position.y, 0));
     }
 
-        private void spawnEnemy()
+    private void spawnEnemy()
     {
         GameObject a = Instantiate(asteroidPrefab) as GameObject;
         a.transform.position = new Vector3(Random.Range(-5, 5), screenBounds.y+20, 0);
