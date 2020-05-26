4dee2bf68c154174a36a0b854302788963f0ac0d
melissandeglt <melissande.gaillot@outlook.fr>
2020-05-26 12:44:12 +0200

    :bug: Fixing asteroid x position

--- Assets/Scenes/Level/Script/deployAsteroid.cs
+++ Assets/Scenes/Level/Script/deployAsteroid.cs
@@ -7,7 +7,6 @@ public class DeployAsteroid : MonoBehaviour
     public GameObject asteroidPrefab;
     public float respawnTime = 1.0f;
     private Vector3 screenBounds;
-    private float coordX = (float)Screen.width;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +16,13 @@ public class DeployAsteroid : MonoBehaviour
 
     void Update()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(coordX, Camera.main.transform.position.y, 0));
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0));
     }
 
     private void spawnEnemy()
     {
         GameObject a = Instantiate(asteroidPrefab) as GameObject;
-        a.transform.position = new Vector3(Random.Range(-5, 5), screenBounds.y+20, 0);
+        a.transform.position = new Vector3(Random.Range(screenBounds.x-5, screenBounds.x+5), screenBounds.y+15, 0);
     }
 
     IEnumerator asteroidWave()
