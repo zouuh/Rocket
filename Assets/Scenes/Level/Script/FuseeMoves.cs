a7454cd3a1797bf97978262db055ced0c4cfa09d
zouuh <zoedur@gmail.com>
2020-05-26 14:02:01 +0200

    :sparkles: Add speed steps

--- Assets/Scenes/Level/Script/FuseeMoves.cs
+++ Assets/Scenes/Level/Script/FuseeMoves.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class FuseeMoves : MonoBehaviour
 {
+    public Transform rocket;
 	public Rigidbody rb;
     private float speed = 10.0f;
+    private float step = 10;
 	private float sidewaysForce = 5f;
 
     void Update() {
         if(!FindObjectOfType<GameManager>().getGameHasEnded()) {
+            if (rocket.position.y - 6 >= step * 5) {
+                speed *= 1.2f;
+                step *= 5;
+            }
+
             rb.velocity = new Vector3(0, speed, 0);
 
             if (Input.GetKey("q")) {
