5a2d028aaa6db0626b1bce3d2e1e2e680c6d67a2
zouuh <zoedur@gmail.com>
2020-05-26 12:25:54 +0200

    :bug: Fixing lateral movement

--- Assets/Scenes/Level/Script/FuseeMoves.cs
+++ Assets/Scenes/Level/Script/FuseeMoves.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class FuseeMoves : MonoBehaviour
 {
-    //Ref to rigidBody
 	public Rigidbody rb;
     private float speed = 10.0f;
-    //private float forwardForce = 100.0f;
-	private float sidewaysForce = 500f;
+	private float sidewaysForce = 5f;
 
-    // Fixed is beacause we are using it to mess with physics
     void FixedUpdate() {
-        //Add forward force
-        // rb.AddForce(0, forwardForce * Time.deltaTime/6, 0);
-        // rb = this.GetComponent<Rigidbody>();
         rb.velocity = new Vector3(0, speed, 0);
 
         if (Input.GetKey("q")) {
-        	rb.AddForce(sidewaysForce * Time.deltaTime/2, 0, 0);
+            rb.velocity = new Vector3(sidewaysForce, speed, 0);
         }
        	if(Input.GetKey("f")) {
-        	rb.AddForce((sidewaysForce * Time.deltaTime)/3, 0, 0);
+            rb.velocity = new Vector3(sidewaysForce/2, speed, 0);
         }
         if(Input.GetKey("j")) {
-        	rb.AddForce(-(sidewaysForce * Time.deltaTime)/3, 0, 0);
+            rb.velocity = new Vector3(-sidewaysForce/2, speed, 0);
         }
         if(Input.GetKey("m")) {
-        	rb.AddForce(-sidewaysForce * Time.deltaTime/2, 0, 0);
+            rb.velocity = new Vector3(-sidewaysForce, speed, 0);
         }
     }
 }
