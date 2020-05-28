86cfa38f0d0c6e5c1dc6ea41b9e22aaa03dadc31
melissandeglt <melissande.gaillot@outlook.fr>
2020-05-28 17:32:38 +0200

    :rocket: Add asteroid random scale

--- Assets/Scenes/Level/Script/deployAsteroid.cs
+++ Assets/Scenes/Level/Script/deployAsteroid.cs
@@ -7,6 +7,7 @@ public class DeployAsteroid : MonoBehaviour
     public GameObject asteroidPrefab;
     public float respawnTime = 1.0f;
     private Vector3 screenBounds;
+    private Vector3 scale;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +18,14 @@ public class DeployAsteroid : MonoBehaviour
     void Update()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0));
+        scale = new Vector3(Random.Range(0.4f, 0.8f), Random.Range(0.4f, 0.8f), Random.Range(0.4f, 0.8f));
     }
 
     private void spawnEnemy()
     {
         GameObject a = Instantiate(asteroidPrefab) as GameObject;
-        a.transform.position = new Vector3(Random.Range(screenBounds.x-5, screenBounds.x+5), screenBounds.y+10, 0);
+        a.transform.position = new Vector3(Random.Range(screenBounds.x-5, screenBounds.x+5), screenBounds.y+15, 0); 
+        a.transform.localScale = scale;
     }
 
     IEnumerator asteroidWave()
