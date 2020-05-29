e03c5fb62dce47575c732d766816ba8f8cbbd4ce
zouuh <zoedur@gmail.com>
2020-05-29 16:53:24 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scenes/Level/Script/deployAsteroid.cs
+++ Assets/Scenes/Level/Script/deployAsteroid.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DeployAsteroid : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class DeployAsteroid : MonoBehaviour
     public float respawnTime = 1.0f;
     private Vector3 screenBounds;
     private Vector3 scale;
+
+    private float var;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +24,21 @@ public class DeployAsteroid : MonoBehaviour
         scale = new Vector3(Random.Range(0.4f, 0.8f), Random.Range(0.4f, 0.8f), Random.Range(0.4f, 0.8f));
     }
 
+    private float loiBinomiale(int n, float p) {
+        var = 0;
+        for(int i=0; i<n; i++) {
+            float rand = (float)Random.Range(0, 1f);
+            if(rand > p) {
+                var++;
+            }
+        }
+        return var/10;
+    }
+
     private void spawnEnemy()
     {
         GameObject a = Instantiate(asteroidPrefab) as GameObject;
-        a.transform.position = new Vector3(Random.Range(screenBounds.x-5, screenBounds.x+5), screenBounds.y+15, 0); 
+        a.transform.position = new Vector3(loiBinomiale(100, 0.5f) + screenBounds.x - 5, screenBounds.y+10, 0);
         a.transform.localScale = scale;
     }
 
@@ -32,6 +46,9 @@ public class DeployAsteroid : MonoBehaviour
     {
         while (true)
         {
+            // PREMIERE VARIABLE ALEATOIRE : temps de respawn -> loi uniforme sur l'intervalle [0, 0.5]
+            respawnTime = (float)Random.Range(0, 0.5f) + 0.6f;
+
             yield return new WaitForSeconds(respawnTime);
             if(FindObjectOfType<GameManager>().getGameHasBegin()) {
                 spawnEnemy();
