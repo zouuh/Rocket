f78520a23a10be0c6b7723faae78a52676aa06c2
zouuh <zoedur@gmail.com>
2020-06-01 11:27:56 +0200

    :bug: Fixing camera moves

--- Assets/Scenes/Level/Script/CameraMoves.cs
+++ Assets/Scenes/Level/Script/CameraMoves.cs
@@ -13,8 +13,8 @@ public class CameraMoves : MonoBehaviour
     	    transform.position = player.position + offset; 
         }
         else if(transform.position.y < 40) {
-            offset.y += (float)0.0175;
-            offset.z -= (float)0.045;
+            offset.y += (float)0.009;
+            offset.z -= (float)0.022;
             transform.position = player.position + offset;
         } 
         else {
